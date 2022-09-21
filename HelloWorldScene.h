82b2bd03dc08b1a44db6abe352ebcc69ed735608
doniwahyudi <105205762+doniwahyudi2@users.noreply.github.com>
2022-09-21 12:06:20 +0700

    Create HelloWorldScene.h

---
+++ HelloWorldScene.h
@@ -0,0 +1,5 @@
+    bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+    void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
+    void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event);
+    void screenshot(Ref* Sender);
+    void afterCapture(bool succeed, const std::string& outputFile);
